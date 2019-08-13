c484af0330077500181a355a1c7cf08ea63b0c48
Rodrigo Gomez <rodrigosgg94@gmail.com>
2019-08-13 16:37:11 -0400

    Primeros ejercicios

--- arbol.py
+++ arbol.py
@@ -17,21 +17,6 @@ node4 = Node('Bonos Chile', amount=200000.0, parent=node1)
 node5 = Node('Acciones US', amount=500000.0, parent=node2)
 node6 = Node('Acciones Chile', amount=200000.0, parent=node2)
 
-"""
-Implementa la función print_tree, que recibe como único argumento el nodo
-raíz de un árbol e imprime los valores de name y amount de cada nodo
-de forma ordenada.
-Ejemplo:
->> print_tree(root)
-Total: 1000000.0
-  Bonos: 300000.0
-    Bonos US: 100000.0
-    Bonos Chile: 200000.0
-  Acciones: 700000.0
-    Acciones US: 500000.0
-    Acciones Chile: 200000.0
->>
-"""
 
 
 def print_tree(root_node):
--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,3 @@
-# Implementa la función fib(n) que retorna el n-ésimo número de la serie
-# de Fibonacci:
-# fib(1) = 1
-# fib(2) = 1
-# fib(n) = fib(n - 1) + fib(n - 2), para todo n > 2.
-
-
 def fib(n):
     if n < 2:
         result = n
@@ -14,8 +7,6 @@ def fib(n):
 
 
 if __name__ == "__main__":
-    # Prueba de la función: imprime los primeros 10 números en una línea
-    # separados por coma y espacio.
     prueba_fib = ''
     for i in range(1,11):
         result = fib(i)
